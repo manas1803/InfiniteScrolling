86536072298afb6243cbc106c8231c51d662ad26
manas1803 <manasmonupant@gmail.com>
2024-07-18 23:58:04 +0530

    Updating hasMore code logic

--- infinite-scrolling/src/App.js
+++ infinite-scrolling/src/App.js
@@ -1,44 +1,47 @@
-import { useCallback, useRef, useState } from "react";
-import useAuthorList from "./hooks/useAuthorList";
+import React, { useCallback, useRef, useState } from "react";
+import { useAuthorList } from "./hooks/useAuthorList";
 
 const App = () => {
   const [limit, setLimit] = useState(10);
-  const [page] = useState(1);
-  const [loading, error, hasMore, authorQuotes] = useAuthorList(limit, page);
+  const [pageNumber] = useState(1);
 
-  const observer = useRef(null);
-
-  const lastElementRef = useCallback(
-    (element) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setLimit((prevLimit) => prevLimit + 10);
-        }
-      });
-
-      if (element) observer.current.observe(element);
-    },
-    [loading, hasMore]
+  const [loading, error, authorList, hasMore] = useAuthorList(
+    limit,
+    pageNumber
   );
+
+  const observer = useRef(null)
+  
+  const lastElementRef = useCallback((element)=>{
+    if(loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries=>{
+      if(entries[0].isIntersecting && hasMore){
+        setLimit(prev=>prev+10)
+      }
+    })
+
+    if(element) observer.current.observe(element)
+    
+  },[loading,hasMore])
 
   return (
     <>
-      {authorQuotes.map((authorQuote, index) => {
-        if (authorQuotes.length === index + 1) {
+      {authorList.map((authors,index) => {
+        if(authorList.length===index+1){
           return (
-            <div key={authorQuote.id} ref={lastElementRef}>
-              <blockquote>{authorQuote.quote}</blockquote>
-              <footer>{authorQuote.author}</footer>
+            <div key={authors.id} ref={lastElementRef}>
+              <blockquote>{authors.quote}</blockquote>
+              <footer>{authors.author}</footer>
+              <hr />
             </div>
           );
         }
         return (
-          <div key={authorQuote.id}>
-            <blockquote>{authorQuote.quote}</blockquote>
-            <footer>{authorQuote.author}</footer>
+          <div key={authors.id}>
+            <blockquote>{authors.quote}</blockquote>
+            <footer>{authors.author}</footer>
+            <hr />
           </div>
         );
       })}
--- infinite-scrolling/src/hooks/useAuthorList.js
+++ infinite-scrolling/src/hooks/useAuthorList.js
@@ -2,32 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL } from "../common/constants";
 
-const useAuthorList = (authorListLimit, pageNumber) => {
+export const useAuthorList = (listLimit, pageNumber) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [authorList, setAuthorList] = useState([]);
   const [hasMore, setHasMore] = useState(false);
-  const [authorQuotes, setAuthorQuotes] = useState([]);
 
   useEffect(() => {
     setLoading(true);
+
     setTimeout(() => {
       axios({
         method: "GET",
         url: API_URL,
-        params: { page: pageNumber, limit: authorListLimit },
+        params: { page: pageNumber, limit: listLimit },
       })
         .then((res) => {
-          setAuthorQuotes(res.data.data);
-          setHasMore(res.data.data.length > 0);
-          setLoading(false);
+          setAuthorList(res.data.data);
+          setLoading(false)
+          setHasMore(res.data.data.length===listLimit)
         })
-        .catch((error) => {
-          setError(true);
-        });
+        .catch(() => setError(true));
     }, 1000);
-  }, [authorListLimit, pageNumber]);
+  }, [listLimit, pageNumber]);
 
-  return [loading, error, hasMore, authorQuotes];
+  return [loading,error,authorList,hasMore]
 };
-
-export default useAuthorList;
